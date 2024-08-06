9b8ff6220946df995361f2858e98ab021b02425d
Alexey Ugarin <alexugarin@gmail.com>
2024-08-06 21:17:47 +0400

    create vue-ts project

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,7 @@
-import { resolve } from 'path'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
-  resolve: {
-    alias: {
-      '@': resolve('src')
-    }
-  }
+  plugins: [vue()],
 })
