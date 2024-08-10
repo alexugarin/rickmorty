2f0d704fc344eab6d5082feeeee9d2a4f150e483
Alexey Ugarin <alexugarin@gmail.com>
2024-08-10 23:34:19 +0400

    add index.css

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+	purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   content: [],
+	darkMode: false,
   theme: {
     extend: {},
   },
